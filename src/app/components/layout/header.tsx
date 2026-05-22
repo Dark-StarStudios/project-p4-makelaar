58cb846c97a7237aa33b7187f85643d5ab3e397e
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2026-05-22 11:48:16 +0200

    Toegevoegd voorbeelden van componenten
    - header
    - buttnon

---
+++ src/app/components/layout/header.tsx
@@ -0,0 +1,11 @@
+import Button from "../ui/button"
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Makelaarskantoor</h1>
+      <Button>Click me</Button>
+    </header>
+  )
+}
+export default Header
---
+++ src/app/components/ui/button.tsx
@@ -0,0 +1,4 @@
+function Button({ children }: { children: React.ReactNode }) {
+  return <button className="button bg-amber-600">{children}</button>
+}
+export default Button
