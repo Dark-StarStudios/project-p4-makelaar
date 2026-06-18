8545f86c3a2e701e74ba537ed06f428024d7d79f
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2026-06-18 14:28:53 +0200

    Verwijder de oude componenten

--- src/app/components/ui/button.tsx
+++
@@ -1,6 +0,0 @@
-
-
-function Button({ children , className }: { children: React.ReactNode; className?: string }) {
-  return <button className={`button ${className}`}>{children}</button>
-}
-export default Button
